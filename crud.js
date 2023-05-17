000637c4dbbb619151f66ae63725ca4ff67c7f6d
pedrogb1 <pedrogbranco1@gmail.com>
2023-05-16 22:49:11 -0300

    storage

--- crud.js
+++ crud.js
@@ -12,12 +12,14 @@ window.addEventListener("load", () => {
 
 function cadastrar() {
     const modal = new bootstrap.Modal(document.querySelector("#exampleModal"));
+    // const modal = bootstrap.Modal.getInstance(document.querySelector("#exampleModal"));
     console.log("Cadastrar");
     let titulo = document.querySelector("#titulo").value;
     let genero = Array.from(document.querySelectorAll("#genero input[type='checkbox']:checked")).map(checkbox => checkbox.value);
     let nota = document.querySelector("#nota").value;
     let sinopse = document.querySelector("#sinopse").value;
     let elenco = document.querySelector("#elenco").value;
+    let visto = false
     console.log(titulo, genero, nota, sinopse, elenco);
 
     const filme = {
@@ -26,7 +28,8 @@ function cadastrar() {
         genero,
         nota,
         sinopse,
-        elenco
+        elenco,
+        visto
     };
     if (filme.titulo.length === 0) {
         document.querySelector("#titulo").classList.add("is-invalid");
@@ -38,17 +41,49 @@ function cadastrar() {
 
     document.querySelector("#titulo").value = "";
 
-    localStorage.setItem("lista_filmes", JSON.stringify(lista_filmes));
+    salvar()
 
     modal.hide();
 }
+
+function salvar(){
+    localStorage.setItem("lista_filmes", JSON.stringify(lista_filmes));
+}
+
 
 function apagar(botao, id) {
     botao.parentNode.parentNode.remove();
     deleteCard(id);
 }
 
+function concluir(id){
+    let filme_encontrado = lista_filmes.find((filme) => {
+        console.log(id)
+
+        return filme.id == id
+    })
+    filme_encontrado.visto = true
+    console.log(filme_encontrado.visto)
+    salvar()
+}
+
+
+document.querySelector("#busca").addEventListener("keyup", () =>{
+    lista_filmes = JSON.parse(localStorage.getItem("lista_filmes")) || []
+    const titulo = document.querySelector("#busca").value
+    lista_filmes = lista_filmes.filter(filme => filme.titulo.includes(titulo))
+    } );
+    
+
+document.querySelector("#concluidas").addEventListener("click", () =>{
+lista_filmes = JSON.parse(localStorage.getItem("lista_filmes")) || []
+lista_filmes = lista_filmes.filter(filme => filme.visto)
+} );
+
+
+
 function gerarCard(filme) {
+    const status = (filme.visto) ? "visto" : ""
     return `<div class="col-12 col-md-6 col-lg-3" id="${filme.id}">
                 <div class="card">
                     <div class="card-header">
@@ -59,10 +94,10 @@ function gerarCard(filme) {
                         <p>
                             ${filme.genero.map(genre => `<span class="badge bg-warning">${genre}</span>`).join(' ')}
                         </p>
-                        <a href="#" class="btn btn-success">
+                        <a href="#" onClick="concluir( ${filme.id})" class="btn btn-success" ${status})>
                             <i class="bi bi-check-lg"></i>
                         </a>
-                        <a href="#" class="btn btn-danger" onClick="apagar(this, ${filme.id})">
+                        <a href="#" onClick="apagar(this, ${filme.id})" class="btn btn-danger" >
                             <i class="bi bi-trash"></i>
                         </a>
                     </div>
@@ -72,5 +107,5 @@ function gerarCard(filme) {
 
 function deleteCard(id) {
     lista_filmes = lista_filmes.filter(filme => filme.id !== id);
-    localStorage.setItem("lista_filmes", JSON.stringify(lista_filmes));
+    salvar();
 }
