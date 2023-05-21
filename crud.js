17a7f248ad27418f0092f493a6f80f4a8511034e
pedrogb1 <86267784+pedrogb1@users.noreply.github.com>
2023-05-21 18:14:37 -0300

    css, filter completed, edit added

--- crud.js
+++ crud.js
@@ -1,4 +1,3 @@
-let id = 0;
 let lista_filmes = [];
 
 document.querySelector("#salvar").addEventListener("click", cadastrar);
@@ -8,19 +7,35 @@ window.addEventListener("load", () => {
     lista_filmes.forEach((filme) => {
         document.querySelector("#filmes").innerHTML += gerarCard(filme);
     });
+
+    const generos = document.querySelectorAll(".genero");
+    generos.forEach((genero) => {
+        genero.addEventListener("click", () => {
+            const termoGenero = genero.dataset.genero;
+            const filmesFiltrados = lista_filmes.filter(filme => filme.genero.includes(termoGenero));
+
+            const filtroAplicado = document.querySelector("#filtro-aplicado");
+            filtroAplicado.textContent = `Filtrando por: ${termoGenero}`;
+
+            const filmesContainer = document.querySelector("#filmes");
+            filmesContainer.innerHTML = "";
+
+            filmesFiltrados.forEach(filme => {
+                filmesContainer.innerHTML += gerarCard(filme);
+            });
+        });
+    });
 });
 
 function cadastrar() {
     const modal = new bootstrap.Modal(document.querySelector("#exampleModal"));
-    // const modal = bootstrap.Modal.getInstance(document.querySelector("#exampleModal"));
-    console.log("Cadastrar");
+
     let titulo = document.querySelector("#titulo").value;
-    let genero = Array.from(document.querySelectorAll("#genero input[type='checkbox']:checked")).map(checkbox => checkbox.value);
+    let genero = [...document.querySelectorAll("#genero input[type='checkbox']:checked")].map(checkbox => checkbox.value);
     let nota = document.querySelector("#nota").value;
     let sinopse = document.querySelector("#sinopse").value;
     let elenco = document.querySelector("#elenco").value;
-    let visto = false
-    console.log(titulo, genero, nota, sinopse, elenco);
+    let visto = false;
 
     const filme = {
         id: Date.now(),
@@ -31,35 +46,58 @@ function cadastrar() {
         elenco,
         visto
     };
-    if (filme.titulo.length === 0) {
-        document.querySelector("#titulo").classList.add("is-invalid");
-        return;
+
+    const editarId = document.querySelector("#salvar").dataset.id;
+    if (editarId) {
+        // Edição do filme existente
+        const filmeExistente = lista_filmes.find(filme => filme.id == editarId);
+        if (filmeExistente) {
+            filme.id = editarId; // Mantém o mesmo ID do filme
+            const index = lista_filmes.indexOf(filmeExistente);
+            lista_filmes[index] = filme;
+        }
+    } else {
+        // Novo filme
+        lista_filmes.push(filme);
     }
-    lista_filmes.push(filme);
 
-    document.querySelector("#filmes").innerHTML += gerarCard(filme);
-
+    // Limpar campos do formulário
     document.querySelector("#titulo").value = "";
+    document.querySelectorAll("#genero input[type='checkbox']:checked").forEach(checkbox => {
+        checkbox.checked = false;
+    });
+    document.querySelector("#nota").value = "5";
+    document.querySelector("#sinopse").value = "";
+    document.querySelector("#elenco").value = "";
 
-    salvar()
-
+    salvar();
     modal.hide();
+
+    exibirFilmes();
 }
 
-function salvar(){
+function exibirFilmes() {
+    const filmesContainer = document.querySelector("#filmes");
+    filmesContainer.innerHTML = "";
+
+    lista_filmes.forEach(filme => {
+        filmesContainer.innerHTML += gerarCard(filme);
+    });
+}
+
+function salvar() {
     localStorage.setItem("lista_filmes", JSON.stringify(lista_filmes));
 }
-
 
 function apagar(botao, id) {
     botao.parentNode.parentNode.remove();
     deleteCard(id);
 }
 
-function concluir(id){
+
+function concluir(id) {
     let filme_encontrado = lista_filmes.find((filme) => {
         console.log(id)
-
         return filme.id == id
     })
     filme_encontrado.visto = true
@@ -67,45 +105,121 @@ function concluir(id){
     salvar()
 }
 
+document.querySelector("#busca").addEventListener("keyup", () => {
+    const termoBusca = document.querySelector("#busca").value.trim();
+    const regex = new RegExp(termoBusca, "i"); // Expressão regular case-insensitive
 
-document.querySelector("#busca").addEventListener("keyup", () =>{
-    lista_filmes = JSON.parse(localStorage.getItem("lista_filmes")) || []
-    const titulo = document.querySelector("#busca").value
-    lista_filmes = lista_filmes.filter(filme => filme.titulo.includes(titulo))
-    } );
-    
+    lista_filmes = JSON.parse(localStorage.getItem("lista_filmes")) || [];
+    const filmesFiltrados = lista_filmes.filter(filme => regex.test(filme.titulo));
 
-document.querySelector("#concluidas").addEventListener("click", () =>{
-lista_filmes = JSON.parse(localStorage.getItem("lista_filmes")) || []
-lista_filmes = lista_filmes.filter(filme => filme.visto)
-} );
+    // Limpar a exibição HTML anterior
+    const filmesContainer = document.querySelector("#filmes");
+    filmesContainer.innerHTML = "";
+
+    // Gerar e adicionar cards para os filmes filtrados
+    filmesFiltrados.forEach(filme => {
+        filmesContainer.innerHTML += gerarCard(filme);
+    });
+});
+
+
+document.querySelector("#concluidas").addEventListener("click", () => {
+    lista_filmes = JSON.parse(localStorage.getItem("lista_filmes")) || [];
+    const filmesVistos = lista_filmes.filter(filme => filme.visto);
+
+    const filmesContainer = document.querySelector("#filmes");
+    filmesContainer.innerHTML = "";
+
+    filmesVistos.forEach(filme => {
+        filmesContainer.innerHTML += gerarCard(filme);
+    });
+});
+
+document.querySelector("#home").addEventListener("click", () => {
+    lista_filmes = JSON.parse(localStorage.getItem("lista_filmes")) || [];
+
+    const filmesContainer = document.querySelector("#filmes");
+    filmesContainer.innerHTML = "";
+
+    lista_filmes.forEach(filme => {
+        filmesContainer.innerHTML += gerarCard(filme);
+    });
+
+    const filtroAplicado = document.querySelector("#filtro-aplicado");
+    filtroAplicado.textContent = "Nenhum filtro aplicado";
+});
+
+document.querySelector("#nao-vistos").addEventListener("click", () => {
+    lista_filmes = JSON.parse(localStorage.getItem("lista_filmes")) || [];
+    const filmesNaoVistos = lista_filmes.filter(filme => !filme.visto);
+
+    const filmesContainer = document.querySelector("#filmes");
+    filmesContainer.innerHTML = "";
+
+    filmesNaoVistos.forEach(filme => {
+        filmesContainer.innerHTML += gerarCard(filme);
+    });
+
+    const filtroAplicado = document.querySelector("#filtro-aplicado");
+    filtroAplicado.textContent = "Filtrando por: Não Vistos";
+});
+
 
 
 
 function gerarCard(filme) {
-    const status = (filme.visto) ? "visto" : ""
-    return `<div class="col-12 col-md-6 col-lg-3" id="${filme.id}">
-                <div class="card">
-                    <div class="card-header">
-                        ${filme.titulo}
-                    </div>
-                    <div class="card-body">
-                        <p class="card-text">${filme.sinopse}</p>
-                        <p>
-                            ${filme.genero.map(genre => `<span class="badge bg-warning">${genre}</span>`).join(' ')}
-                        </p>
-                        <a href="#" onClick="concluir( ${filme.id})" class="btn btn-success" ${status})>
-                            <i class="bi bi-check-lg"></i>
-                        </a>
-                        <a href="#" onClick="apagar(this, ${filme.id})" class="btn btn-danger" >
-                            <i class="bi bi-trash"></i>
-                        </a>
-                    </div>
+    const status = filme.visto ? "visto" : "";
+    return `
+        <div class="col-12 col-md-6 col-lg-3" id="${filme.id}">
+            <div class="card">
+                <div class="card-header">
+                    ${filme.titulo}
                 </div>
-            </div>`;
+                <div class="card-body">
+                    <p class="card-text">${filme.sinopse}</p>
+                    <p>
+                        ${filme.genero
+            .map(genre => `<span class="badge bg-warning genero genero-clicavel" data-genero="${genre}">${genre}</span>`)
+            .join(' ')}
+                    </p>
+                    <a href="#" onClick="concluir(${filme.id})" class="btn btn-success ${status}">
+                        <i class="bi bi-check-lg"></i>
+                    </a>
+                    <a href="#" onClick="apagar(this, ${filme.id})" class="btn btn-danger">
+                        <i class="bi bi-trash"></i>
+                    </a>
+                    <button class="btn btn-primary btn-editar" onclick="editarCard(${filme.id})">
+                        Editar
+                    </button>
+                </div>
+            </div>
+        </div>`;
 }
 
 function deleteCard(id) {
     lista_filmes = lista_filmes.filter(filme => filme.id !== id);
     salvar();
+}
+
+function editarCard(id) {
+    const filmeEncontrado = lista_filmes.find(filme => filme.id === id);
+
+    // Verificar se o filme foi encontrado
+    if (filmeEncontrado) {
+        // Preencher os campos do formulário com os dados do filme
+        document.querySelector("#titulo").value = filmeEncontrado.titulo;
+        document.querySelectorAll("#genero input[type='checkbox']").forEach(checkbox => {
+            checkbox.checked = filmeEncontrado.genero.includes(checkbox.value);
+        });
+        document.querySelector("#nota").value = filmeEncontrado.nota;
+        document.querySelector("#sinopse").value = filmeEncontrado.sinopse;
+        document.querySelector("#elenco").value = filmeEncontrado.elenco;
+
+        // Atualizar o ID do filme no botão "Salvar" para identificar a edição
+        document.querySelector("#salvar").dataset.id = id;
+
+        // Abrir o modal de edição
+        const modal = new bootstrap.Modal(document.querySelector("#exampleModal"));
+        modal.show();
+    }
 }
